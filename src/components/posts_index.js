85eae08d3560269f09fbd628c3e18945860069d9
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-05-19 19:28:46 +0200

    Home page and installed redux form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,48 @@
+import _ from "lodash"
 import React, { Component } from 'react';
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
+import { fetchPosts } from "../actions"
+
 
 class PostsIndex extends Component {
+  //Function that will be automatically called when loaded
+  componentDidMount() {
+    this.props.fetchPosts();
+
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.PostsIndex}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
+
   render() {
+    //console.log("First render", this.props.posts)
     return (
       <div>
-        Posts Index
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
+//Whenever we want to consume anything from application level state
+function mapStateToProps(state) {
+  return { posts: state.posts }
+}
 
-export default PostsIndex;
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
